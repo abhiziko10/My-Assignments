9e7187705c5bf9c44db628104fdb0f49b3c7a6f5
Abhishek Banerjee <109202077+abhiziko10@users.noreply.github.com>
2022-07-28 16:36:42 +0530

    Update abstract_assignment.java

--- JAVA/CORE_JAVA/DAY_8_COREJAVA_ASSIGNMENT1/abstract_assignment.java
+++ JAVA/CORE_JAVA/DAY_8_COREJAVA_ASSIGNMENT1/abstract_assignment.java
@@ -21,3 +21,11 @@ public class abstract_assignment {
         v.engine();
     }
 }
+/*
+C:\Users\coditas\.jdks\openjdk-18.0.1.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.3\lib\idea_rt.jar=64224:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.1.3\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\coditas\IdeaProjects\CORE JAVA_DAY 2\out\production\CORE JAVA_DAY 2" DAY_8_COREJAVA_ASSIGNMENT1.abstract_assignment
+Car has good engine
+Truck has bad engine
+
+Process finished with exit code 0
+
+ */
