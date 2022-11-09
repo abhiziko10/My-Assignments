a13cbc4f11591c7cd69c64f08baae14ecec33428
Abhishek Banerjee <109202077+abhiziko10@users.noreply.github.com>
2022-11-09 15:07:13 +0530

    Add files via upload

---
+++ HACKERRANK/PIPEvaluationCode/November_PIP_Evaluation/Lecture.java
@@ -0,0 +1,28 @@
+package November_PIP_Evaluation;
+
+public class Lecture {
+    public static void main(String[] args) {
+        int arrivalTime[]={12,1,2,3,5,6};
+        int hours[]={2,1,1,3,4,3};
+         arrivalTime[0] =1;
+        int count=1;
+        int nextLecture;
+
+        if(arrivalTime[0]==12){
+            nextLecture=hours[0];
+        }else{
+            nextLecture=arrivalTime[0]+hours[0];
+        }
+        for(int i=1;i<arrivalTime.length;i++) {
+            if (arrivalTime[i] >= nextLecture) {
+                count++;
+                nextLecture = nextLecture + hours[i];
+            }
+        }
+        System.out.println("Number of classes conducted are:");
+        System.out.println(+count);
+        System.out.println("Number of classes that are cancelled are");
+        System.out.println((arrivalTime.length)-count);
+    }
+}
+
