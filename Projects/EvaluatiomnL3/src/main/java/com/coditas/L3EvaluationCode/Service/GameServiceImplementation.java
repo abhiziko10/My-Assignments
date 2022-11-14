41d12cb270d985491990a7693fae585362b9da4a
Abhishek Banerjee <109202077+abhiziko10@users.noreply.github.com>
2022-11-14 14:56:18 +0530

    Update GameServiceImplementation.java

--- Projects/EvaluatiomnL3/src/main/java/com/coditas/L3EvaluationCode/Service/GameServiceImplementation.java
+++ Projects/EvaluatiomnL3/src/main/java/com/coditas/L3EvaluationCode/Service/GameServiceImplementation.java
@@ -2,6 +2,9 @@ package com.coditas.L3EvaluationCode.Service;
 
 import com.coditas.L3EvaluationCode.Entity.MatchEntity;
 import com.coditas.L3EvaluationCode.Entity.PlayerEntity;
+import com.coditas.L3EvaluationCode.Exception.PositionOccupiedException;
+import com.coditas.L3EvaluationCode.Model_DTO.GameDTO;
+import com.coditas.L3EvaluationCode.Model_DTO.PlayerMoveDTO;
 import com.coditas.L3EvaluationCode.Model_DTO.StartMatchDTO;
 import com.coditas.L3EvaluationCode.Repository.MatchRepository;
 import com.coditas.L3EvaluationCode.Repository.PlayerRepository;
@@ -17,28 +20,44 @@ public class GameServiceImplementation implements GameService {
     PlayerRepository playerRepository;
 
     @Autowired
-    PlayerEntity playerEntity;
-
+    PlayerMoveDTO playerMoveDto;
+    @Autowired
+    GameDTO gameDTO;
     @Autowired
     MatchEntity matchEntity;
+    @Autowired
+    StartMatchDTO startMatchDTO;
+
+
 
     @Autowired
     MatchRepository matchRepository;
 
     @Override
-    public MatchEntity createNewMatch(StartMatchDTO startMatchDto) {
-        PlayerEntity playerOne = playerRepository.findById((int) startMatchDto.getPlayerOneId()).orElse(null);
-        PlayerEntity playerTwo = playerRepository.findById((int) startMatchDto.getPlayerTwoId()).orElse(null);
+    public MatchEntity createNewMatch(StartMatchDTO startMatchDto){
+        PlayerEntity playerOne = playerRepository.findById(startMatchDTO.getPlayerOneId()).orElse(null);
+        PlayerEntity playerTwo = playerRepository.findById(startMatchDTO.getPlayerTwoId()).orElse(null);
 
+
+        playerOne.setPlayerStatus("In a game");
+        playerTwo.setPlayerStatus("in a game");
         List<PlayerEntity> playersEnrolled = new ArrayList<>();
         playersEnrolled.add(playerOne);
         playersEnrolled.add(playerTwo);
         matchEntity.setPlayers(playersEnrolled);
-        matchEntity.setMatchName(startMatchDto.getMatchName());
+        matchEntity.setMatchName(startMatchDTO.getMatchName());
         playerRepository.save(playerOne);
         playerRepository.save(playerTwo);
         return matchRepository.save(matchEntity);
     }
 
 
+    public PlayerMoveDTO playerMove (int playerOneMove, int playerTwoMove){
+        if (playerOneMove == playerTwoMove){
+            throw  new PositionOccupiedException();
+        }
+        playerMoveDto.setPlayerOneMove(playerOneMove);
+        playerMoveDto.setPlayerTwoMove(playerTwoMove);
+    }
+     
 }
